1d75322914884fc618997e9dba56ac9873be42d4
Daniel Kollmann <dankolle@mail.de>
2024-01-20 02:08:45 +0900

    Fixed ESCAnimationResource asset creation in editor.

--- addons/escoria-core/game/core-scripts/resources/esc_animationresource.gd
+++ addons/escoria-core/game/core-scripts/resources/esc_animationresource.gd
@@ -10,22 +10,22 @@ class_name ESCAnimationResource
 # start_angle must be between 0 and 360.
 # Angles 0 and 360 are the same and correspond to UP/NORTH,
 # 90 is RIGHT/EAST, 180 is DOWN/SOUTH, 270 is LEFT/WEST etc.
-@export var dir_angles: Array = []: set = set_dir_angles
+@export var dir_angles: Array[ESCDirectionAngle] = []: set = set_dir_angles
 
 # Array of animations for each direction, from UP to RIGHT_UP clockwise
 # [animation_name, scale]: scale parameter can be set to -1 to mirror
 # the animation
-@export var directions: Array = []: set = set_directions
+@export var directions: Array[ESCAnimationName] = []: set = set_directions
 
 # Array containing the idle animations for each direction (in the
 # order defined by dir_angles): scale parameter can be set to -1 to mirror
 # the animation
-@export var idles: Array = []: set = set_idles
+@export var idles: Array[ESCAnimationName] = []: set = set_idles
 
 # Array containing the speak animations for each direction (in the
 # order defined by dir_angles): scale parameter can be set to -1 to mirror
 # the animation
-@export var speaks: Array = []: set = set_speaks
+@export var speaks: Array[ESCAnimationName] = []: set = set_speaks
 
 
 # Sets the dir_angles property.
